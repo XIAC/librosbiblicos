23d93f0d26105248d21e4ce3de12ae7c27a978c1
XIAC <x.i.a.c.1504@gmail.com>
2024-05-12 20:00:38 -0400

    ruta,modelo y controlador versiculos

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const libroRutas = require('./rutas/libroRutas');
+const versiculosRutas = require('./rutas/versiculoRutas');
 const app = express();
 //middleware
 app.use(express.json());
 //Rutas
-app.use('/api/libros',libroRutas);
+app.use('/api/libros', libroRutas);
+app.use('/api/versiculos', versiculosRutas);
+
 //levantar el servidor
 const PORT = 5000;
 app.listen(PORT, () => {
